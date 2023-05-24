7c3f0cb90e73d22981271342e5c90686b7386c4d
Max Valek <valek.maxwell@gmail.com>
2023-05-24 13:53:58 -0500

    Initial Commit

---
+++ SlidingTabs/SlidingTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingTabsApp.swift
+//  SlidingTabs
+//
+//  Created by Max Valek on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SlidingTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
